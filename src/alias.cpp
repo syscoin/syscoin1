syscoin/syscoin1

commit 65228986711dddf288d15ed05b2d7b2d46e66998

Closer

src/alias.cpp
@@ -586,17 +586,10 @@ bool CNameDB::ReconstructNameIndex(CBlockIndex *pindexRescan) {
                     stringFromVch(vchValue).c_str(),
                     tx.GetHash().ToString().c_str(), 
                     nHeight, nTheFee / COIN);
-
-            // add alias to master alias index, add it to my names map
-            // if it is owned by this client
-            vecAliasIndex.push_back(vvchArgs[0]);
-            if(IsAliasMine(tx)) {
-                mapMyAliases[vvchArgs[0]] = tx.GetHash();
             }
         }
         pindex = pindex->pnext;
         Flush();
-    }
     }
     return true;
 }

src/cert.cpp
@@ -172,7 +172,6 @@ bool CCertDB::ReconstructCertIndex(CBlockIndex *pindexRescan) {
             // decode the certissuer op, params, height
             bool o = DecodeCertTx(tx, op, nOut, vvchArgs, nHeight);
             if (!o || !IsCertOp(op)) continue;
-
             if (op == OP_CERTISSUER_NEW) continue;
 
             vector<unsigned char> vchCertIssuer = vvchArgs[0];
@@ -243,17 +242,11 @@ bool CCertDB::ReconstructCertIndex(CBlockIndex *pindexRescan) {
             // master index
             int64 nTheFee = GetCertNetFee(tx);
             InsertCertFee(pindex, tx.GetHash(), nTheFee);
-            vecCertIssuerIndex.push_back(vvchArgs[0]);
-            if (!pcertdb->WriteCertIndex(vecCertIssuerIndex))
-                return error("ReconstructCertIndex() : failed to write index to certissuer DB");
 
-            if(IsCertMine(tx)) {
-                if(op == OP_CERT_NEW || op == OP_CERT_TRANSFER) {
-                    mapMyCertItems[vvchArgs[1]] = tx.GetHash();
-                    if(mapMyCertIssuers.count(vchCertIssuer))
-                        mapMyCertIssuers[vchCertIssuer] = tx.GetHash();
-                }
-                else mapMyCertIssuers[vchCertIssuer] = tx.GetHash();
+            if(op == OP_CERTISSUER_ACTIVATE) {
+                vecCertIssuerIndex.push_back(vvchArgs[0]);
+                if (!pcertdb->WriteCertIndex(vecCertIssuerIndex))
+                    return error("ReconstructCertIndex() : failed to write index to certissuer DB");
             }
 
             printf( "RECONSTRUCT CERT: op=%s certissuer=%s title=%s qty=%d hash=%s height=%d fees=%llu\n",
@@ -491,11 +484,14 @@ bool IsCertMine(const CTransaction& tx, const CTxOut& txout,
         return false;
 
     vector<vector<unsigned char> > vvch;
-    int op;
+    int op, nOut;
 
-    if (!DecodeCertScript(txout.scriptPubKey, op, vvch))
+    bool good = DecodeCertTx(tx, op, nOut, vvch, -1);
+    if (!good) {
+        error( "IsCertMine() : no output out script in cert tx %s\n",
+                tx.ToString().c_str());
         return false;
-
+    }
     if(!IsCertOp(op))
         return false;
 
@@ -1266,17 +1262,11 @@ bool CheckCertInputs(CBlockIndex *pindexBlock, const CTransaction &tx,
                     if (!pcertdb->WriteCertIssuer(vvchArgs[0], vtxPos))
                         return error( "CheckCertInputs() : failed to write to certissuer DB");
 
-                    // write certissuer to certissuer index if it isn't there already
-                    bool bFound = false;
-                    BOOST_FOREACH(vector<unsigned char> &vch, vecCertIssuerIndex) {
-                        if(vch == vvchArgs[0]) {
-                            bFound = true;
-                            break;
-                        }
+                    if(op == OP_CERTISSUER_ACTIVATE) {
+                        vecCertIssuerIndex.push_back(vvchArgs[0]);
+                        if (!pcertdb->WriteCertIndex(vecCertIssuerIndex))
+                            return error("CheckCertInputs() : failed to write index to certissuer DB");
                     }
-                    if(!bFound) vecCertIssuerIndex.push_back(vvchArgs[0]);
-                    if (!pcertdb->WriteCertIndex(vecCertIssuerIndex))
-                        return error("CheckCertInputs() : failed to write index to certissuer DB");
 
                     // compute verify and write fee data to DB
                     int64 nTheFee = GetCertNetFee(tx);
@@ -1314,10 +1304,6 @@ bool CheckCertInputs(CBlockIndex *pindexBlock, const CTransaction &tx,
                             tx.GetHash().ToString().c_str(),
                             nHeight, nTheFee / COIN);
                 }
-            }
-
-            if (pindexBlock->nHeight != pindexBest->nHeight) {
-
             }
         }
     }

src/cert.h
@@ -37,6 +37,7 @@ int GetCertDisplayExpirationDepth(int nHeight);
 int64 GetCertNetworkFee(int seed, int nHeight);
 int64 GetCertNetFee(const CTransaction& tx);
 bool InsertCertFee(CBlockIndex *pindex, uint256 hash, uint64 nValue);
+bool ExtractCertIssuerAddress(const CScript& script, std::string& address);
 
 std::string certissuerFromOp(int op);
 

src/main.cpp
@@ -682,6 +682,7 @@ bool CTransaction::CheckTransaction(CValidationState &state) const {
         }
 
         // offer
+        good = DecodeOfferTx(*this, op, nOut, vvch, iter == 0 ? 0 : -1);
         if(good && IsOfferOp(op)) {
 	        if (vvch[0].size() > MAX_NAME_LENGTH) {
 	            ret[iter] = error("offer transaction with offer title too long");
@@ -722,6 +723,7 @@ bool CTransaction::CheckTransaction(CValidationState &state) const {
         }
 
         // cert
+        good = DecodeCertTx(*this, op, nOut, vvch, iter == 0 ? 0 : -1);
         if(good && IsCertOp(op)) {
 	        if (vvch[0].size() > MAX_NAME_LENGTH) {
 	            ret[iter] = error("cert transaction with cert title too long");
@@ -987,7 +989,6 @@ bool CTxMemPool::accept(CValidationState &state, CTransaction &tx,
 	    int op, nOut;
 
 	    bool good = DecodeSyscoinTx(tx, op, nOut, vvch, -1);
-
 	    if(good && IsAliasOp(op)) {
 	        LOCK(cs_main);
             mapAliasesPending[vvch[0]].insert(tx.GetHash());
@@ -995,6 +996,7 @@ bool CTxMemPool::accept(CValidationState &state, CTransaction &tx,
                 stringFromVch(vvch[0]).c_str());
 	    }
 
+		good = DecodeOfferTx(tx, op, nOut, vvch, -1);
 	    if(good && IsOfferOp(op)) {
 	        LOCK(cs_main);
             if(op == OP_OFFER_ACCEPT || op == OP_OFFER_PAY)
@@ -1006,6 +1008,7 @@ bool CTxMemPool::accept(CValidationState &state, CTransaction &tx,
                 stringFromVch(vvch[0]).c_str());
 	    }
 
+	    good = DecodeCertTx(tx, op, nOut, vvch, -1);
 		if(good && IsCertOp(op)) {
 	        LOCK(cs_main);
 			if(op == OP_CERT_TRANSFER)
@@ -1700,20 +1703,20 @@ bool CTransaction::CheckInputs(CBlockIndex *pindex, CValidationState &state, CCo
 		int op;
 		int nOut;
 
-		bool bDecoded = DecodeSyscoinTx(*this, op, nOut, vvchArgs, pindex->nHeight);
-		if (bDecoded) {
-
-		}
-
-		if(IsAliasOp(op)) {
+		bool bGood = DecodeSyscoinTx(*this, op, nOut, vvchArgs, pindex->nHeight);
+		if(bGood && IsAliasOp(op)) {
 			if (!CheckAliasInputs(pindex, *this, state, inputs, mapTestPool, fBlock, fMiner, bJustCheck))
 				return false;
 		}
-		else if (IsOfferOp(op)) {
+		
+		bGood = DecodeOfferTx(*this, op, nOut, vvchArgs, pindex->nHeight);
+		if (bGood && IsOfferOp(op)) {
 			if (!CheckOfferInputs(pindex, *this, state, inputs, mapTestPool, fBlock, fMiner, bJustCheck))
 				return false;
 		} 
-		else if (IsCertOp(op)) {
+		
+		bGood = DecodeCertTx(*this, op, nOut, vvchArgs, pindex->nHeight);
+		if (bGood && IsCertOp(op)) {
 			if (!CheckCertInputs(pindex, *this, state, inputs, mapTestPool, fBlock, fMiner, bJustCheck))
 				return false;
 		}
@@ -1893,6 +1896,7 @@ bool CBlock::DisconnectBlock(CValidationState &state, CBlockIndex *pindex, CCoin
             }
 
             //*************************
+            good = DecodeOfferTx(tx, op, nOut, vvchArgs, pindex->nHeight);
             if (good && IsOfferOp(op) && op != OP_OFFER_NEW) {
                 string opName = offerFromOp(op);
 				
@@ -1957,6 +1961,7 @@ bool CBlock::DisconnectBlock(CValidationState &state, CBlockIndex *pindex, CCoin
 					pofferdb->EraseOffer(theOffer.vchRand);
 				}
 
+				good = DecodeCertTx(tx, op, nOut, vvchArgs, pindex->nHeight);
                 if (good && IsCertOp(op) && op != OP_CERTISSUER_NEW) {
                     string opName = certissuerFromOp(op);
 

src/offer.cpp
@@ -247,15 +247,6 @@ bool COfferDB::ReconstructOfferIndex(CBlockIndex *pindexRescan) {
 	        if (!pofferdb->WriteOfferIndex(vecOfferIndex))
 	            return error("ReconstructOfferIndex() : failed to write index to offer DB");
 
-            if(IsOfferMine(tx)) {
-                if(op == OP_OFFER_ACCEPT || op == OP_OFFER_PAY) {
-                    mapMyOfferAccepts[vvchArgs[1]] = tx.GetHash();
-                    if(mapMyOffers.count(vchOffer))
-                        mapMyOffers[vchOffer] = tx.GetHash();
-                }
-                else mapMyOffers[vchOffer] = tx.GetHash();
-            }
-
 			printf( "RECONSTRUCT OFFER: op=%s offer=%s title=%s qty=%d hash=%s height=%d fees=%llu\n",
 					offerFromOp(op).c_str(),
 					stringFromVch(vvchArgs[0]).c_str(),

src/qt/certtablemodel.cpp
@@ -71,10 +71,9 @@ public:
         {
             LOCK(wallet->cs_wallet);
             for(unsigned int i=0; i< vecCertIssuerIndex.size(); i++) {
-                vector<unsigned char> cert = vecCertIssuerIndex[i];
-
-                vector<CCertIssuer> vtxPos;
                 CCertIssuer theCertIssuer;
+                vector<unsigned char> cert = vecCertIssuerIndex[i];
+                vector<CCertIssuer> vtxPos;
                 if (pcertdb->ExistsCertIssuer(cert)) {
                     if (!pcertdb->ReadCertIssuer(cert, vtxPos))
                         continue;
@@ -83,24 +82,27 @@ public:
 
                 uint256 txblkhash, txHash = theCertIssuer.txHash;
                 CTransaction tx;
-
                 if(!GetTransaction(txHash, tx, txblkhash, true))
                     continue;
 
-                theCertIssuer.UnserializeFromTx(tx);
+                vector<vector<unsigned char> > vvchArgs;
+                int op, nOut;
+                if (!DecodeCertTx(tx, op, nOut, vvchArgs, -1)) {
+                    continue;
+                }
 
-                CCertItem theCertItem;
+                bool fIsCertItem = op == OP_CERT_NEW || OP_CERT_TRANSFER;
+
                 vector<unsigned char> vchTitle = theCertIssuer.vchTitle;
                 vector<unsigned char> vchRand = theCertIssuer.vchRand;
                 unsigned long nExpDepth = GetCertExpirationDepth(theCertIssuer.nHeight);
 
-                bool fIsCertItem = vtxPos.back().certs.size() != 0;
+                CCertItem theCertItem;
                 if(fIsCertItem) {
                     theCertItem = theCertIssuer.certs.back();
                     vchTitle = theCertItem.vchTitle;
-                    vchRand = theCertItem.vchRand;
+                    vchRand = vchFromString(HexStr(theCertItem.vchRand));
                     nExpDepth = 0;
-
                 }
 
                 cachedCertIssuerTable.append(CertIssuerTableEntry(fIsCertItem ? CertIssuerTableEntry::CertItem : CertIssuerTableEntry::CertIssuer,

src/rpcwallet.cpp
@@ -1070,18 +1070,20 @@ void ListTransactions(const CWalletTx& wtx, const string& strAccount, int nMinDe
                 vector<vector<unsigned char> > vvchArgs;
                 int op,nOut, nTxOut;
                 bool good = DecodeSyscoinTx(wtx, op, nOut, vvchArgs, -1);
-                if(IsAliasOp(op)) {
+                if(good && IsAliasOp(op)) {
                     nTxOut = IndexOfNameOutput(wtx);
                     ExtractAliasAddress(wtx.vout[nTxOut].scriptPubKey, strAddress);
-                } else {
-                    good = DecodeOfferTx(wtx, op, nOut, vvchArgs, -1);
-                    if (!good || !IsOfferOp(op)) {
-                    	JSONRPCError(RPC_WALLET_ERROR, "ListTransactions() : could not decode a syscoin tx");
-                    	return;
-                    }
+                } 
+                good = DecodeOfferTx(wtx, op, nOut, vvchArgs, -1);
+                if(good && IsOfferOp(op)) {
                     nTxOut = IndexOfOfferOutput(wtx);
                     ExtractOfferAddress(wtx.vout[nTxOut].scriptPubKey, strAddress);
-                }
+                } 
+                good = DecodeCertTx(wtx, op, nOut, vvchArgs, -1);
+                if(good && IsCertOp(op)) {
+                    nTxOut = IndexOfCertIssuerOutput(wtx);
+                    ExtractCertIssuerAddress(wtx.vout[nTxOut].scriptPubKey, strAddress);
+                } 
             }
             entry.push_back(Pair("address", strAddress));
             entry.push_back(Pair("category", "send"));

src/wallet.cpp
@@ -366,25 +366,28 @@ void CWallet::WalletUpdateSpent(const CTransaction &tx)
                     NotifyTransactionChanged(this, txin.prevout.hash, CT_UPDATED);
 
                     vector<vector<unsigned char> > vvchArgs;
-                    int op;
-                    int nOut;
+                    int op, nOut;
                     bool good = DecodeSyscoinTx(tx, op, nOut, vvchArgs, -1);
-                    if (good){
-                        if(IsAliasOp(op)) {
-                            const vector<unsigned char> &vchName = vvchArgs[0];
-                            vector<CAliasIndex> vtxPos;
-                            if (paliasdb->ReadAlias(vchName, vtxPos)) {
-                                NotifyAliasListChanged(this, &tx, vtxPos.back(), CT_UPDATED);
-                            }
-                        } else if (IsOfferOp(op)) {
-                            COffer theOffer;
-                            theOffer.UnserializeFromTx(tx);
-                            NotifyOfferListChanged(this, &tx, theOffer, CT_UPDATED);
-                        } else if(IsCertOp(op)) {
-                            CCertIssuer theCI;
-                            theCI.UnserializeFromTx(tx);
-                            NotifyCertIssuerListChanged(this, &tx, theCI, CT_UPDATED);
+                    if(good && IsAliasOp(op)) {
+                        const vector<unsigned char> &vchName = vvchArgs[0];
+                        vector<CAliasIndex> vtxPos;
+                        if (paliasdb->ReadAlias(vchName, vtxPos)) {
+                            NotifyAliasListChanged(this, &tx, vtxPos.back(), CT_UPDATED);
                         }
+                    } 
+                    good = DecodeOfferTx(tx, op, nOut, vvchArgs, -1);
+                    if (good && IsOfferOp(op)) {
+                        COffer theOffer;
+                        theOffer.UnserializeFromTx(tx);
+                        if(!theOffer.IsNull())
+                            NotifyOfferListChanged(this, &tx, theOffer, CT_UPDATED);
+                    }
+                    good = DecodeCertTx(tx, op, nOut, vvchArgs, -1);
+                    if(good && IsCertOp(op)) {
+                        CCertIssuer theCI;
+                        theCI.UnserializeFromTx(tx);
+                        if(!theCI.IsNull())
+                            NotifyCertIssuerListChanged(this, &tx, theCI, CT_UPDATED);
                     }
                 }
             }

commit bca2b6af159afb4890a1d53e3afa25e55f203818

prevent infiniloop while reconstructing aliases this fixes -rescan (previously: hungup) this bug was introduced in https://github.com/syscoin/syscoin/commit/65228986711dddf288d15ed05b2d7b2d46e66998#diff-cfe9e5456ba40db0f1c7f4fdd72ff393

src/alias.cpp
@@ -639,11 +639,12 @@ bool CAliasDB::ReconstructNameIndex(CBlockIndex *pindexRescan) {
                     stringFromVch(vchValue).c_str(),
                     tx.GetHash().ToString().c_str(), 
                     nHeight, nTheFee / COIN);
-            }
-        }
-        pindex = pindex->pnext;
+
+            } /* TX */
+    	    pindex = pindex->pnext;
+        } /* BLOCK */
         Flush();
-    }
+    } /* LOCK */
     return true;
 }
 