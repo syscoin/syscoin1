b029b5861b848ce4a5fd32dcc070242fdc7c51b5
Sebastian Schepis <sschepis@gmail.com>
2014-08-20 02:38:44 -0700

    .

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -765,7 +765,7 @@ Value sendmany(const Array& params, bool fHelp)
     {
         CBitcoinAddress address(s.name_);
         if (!address.IsValid())
-            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, string("Invalid Datacoin address: ")+s.name_);
+            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, string("Invalid Syscoin address: ")+s.name_);
 
         if (setAddress.count(address))
             throw JSONRPCError(RPC_INVALID_PARAMETER, string("Invalid parameter, duplicated address: ")+s.name_);
