15239402914301e60383007ab787ba290056c602
sidhujag <sidhujag@hotmail.com>
2014-08-20 15:28:26 -0700

    revert dogecoin auxpow stuff

--- src/main.h
+++ src/main.h
@@ -1336,7 +1336,7 @@ class CBlockHeader
 {
 public:
     // header
-    static const int CURRENT_VERSION=2;
+    static const int CURRENT_VERSION=BLOCK_VERSION_DEFAULT;
     int nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
