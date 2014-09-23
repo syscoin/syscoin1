51bb2de3ab170b2599f9c544b40712f4d665d25f
Dan Wasyluk <dwasyluk@gmail.com>
2014-09-23 00:15:58 -0400

    OSX Release Tools update

--- contrib/osx-qt-release-tools/OSX_package_dmg.sh
+++ contrib/osx-qt-release-tools/OSX_package_dmg.sh
@@ -5,7 +5,7 @@
 mv /opt/local/lib/db48 /opt/local/lib/lib
 
 #run macdeploy without the DMG command so we can fix the db48 linker manually
-macdeployqt SysCoin-Qt.app/ -verbose=3
+macdeployqt Syscoin-Qt.app/ -verbose=3
 
 #rename port-based db48 to the right name
 mv /opt/local/lib/lib /opt/local/lib/db48
@@ -15,6 +15,6 @@ install_name_tool -id @executable_path/../Frameworks/libdb_cxx-4.8.dylib "Syscoi
 install_name_tool -change /opt/local/lib/db48/libdb_cxx-4.8.dylib @executable_path/../Frameworks/libdb_cxx-4.8.dylib "Syscoin-Qt.app/Contents/MacOS/Syscoin-Qt"
 
 #package the final product into a DMG
-macdeployqt SysCoin-Qt.app/ -dmg
+macdeployqt Syscoin-Qt.app/ -dmg
 
 #end
