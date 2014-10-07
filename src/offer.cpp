3292081adc81fec7b2cfe53d92afe40d1142fbf5
CoderBoo <coderboo@syscoin.org>
2014-10-07 13:24:47 -0700

    fix for wierd shit

--- src/offer.cpp
+++ src/offer.cpp
@@ -322,7 +322,7 @@ int CheckOfferTransactionAtRelativeDepth(CBlockIndex* pindexBlock,
 int GetOfferTxHashHeight(const uint256 txHash) {
 	CDiskTxPos postx;
 	pblocktree->ReadTxIndex(txHash, postx);
-	return GetNameTxPosHeight(postx);
+	return GetOfferTxPosHeight(postx);
 }
 
 uint64 GetOfferFeeSubsidy(unsigned int nHeight) {
@@ -1005,9 +1005,8 @@ bool CheckOfferInputs(CBlockIndex *pindexBlock, const CTransaction &tx,
 		int64 nNetFee;
 
 		// unserialize offer object from txn, check for valid
-		COffer theOffer;
+		COffer theOffer(tx);
 		COfferAccept theOfferAccept;
-		theOffer.UnserializeFromTx(tx);
 		if (theOffer.IsNull())
 			error("CheckOfferInputs() : null offer object");
 
@@ -1357,10 +1356,6 @@ bool CheckOfferInputs(CBlockIndex *pindexBlock, const CTransaction &tx,
 							tx.GetHash().ToString().c_str(), 
 							nHeight, nTheFee / COIN);
 				}
-			}
-
-			if (pindexBlock->nHeight != pindexBest->nHeight) {
-
 			}
 		}
 	}
