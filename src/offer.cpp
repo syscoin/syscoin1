d78fdfcab4bba2c0cbdf8bb718a48513dc574e88
sidhujag <sidhujag@hotmail.com>
2015-07-05 12:31:37 -0700

    Update offer.cpp

--- src/offer.cpp
+++ src/offer.cpp
@@ -2327,7 +2327,7 @@ Value offerwhitelist(const Array& params, bool fHelp) {
 			GetCertAddress(txCert, strAddress);
 			oList.push_back(Pair("cert_address", strAddress));
 			int expires_in = theCert.nHeight + GetCertDisplayExpirationDepth(theCert.nHeight)
-									- pindexBest->nHeight))
+									- pindexBest->nHeight);
 			oList.push_back(Pair("cert_expiresin",expires_in);
 			oList.push_back(Pair("offer_discount_percentage", strprintf("%d%%", entry.nDiscountPct)));
 			oRes.push_back(oList);
