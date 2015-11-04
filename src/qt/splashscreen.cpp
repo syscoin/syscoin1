690ddde3f4aa7ebe4135e0aa4b67985f9588162a
Dan Wasyluk <dwasyluk@gmail.com>
2015-11-04 00:26:40 -0500

    More graphic updates

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -9,8 +9,8 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     QSplashScreen(pixmap, f)
 {
     // set reference point, paddings
-    int paddingLeftCol2         = 176;
-    int paddingTopCol2          = 378;
+    int paddingLeftCol2         = 146;
+    int paddingTopCol2          = 352;
     int line1 = 0;
     int line2 = 13;
     int line3 = 26;
